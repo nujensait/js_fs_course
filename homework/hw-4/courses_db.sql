329f05d67c59ceb0f9a9a6cb37286cc98151a58c
Mikhail Ikonnikov <mishaikon@gmail.com>
2024-02-28 17:13:47 +0300

    GA JS FS HW-4

---
+++ homework/hw-4/courses_db.sql
@@ -0,0 +1,22 @@
+ALTER TABLE `group` DROP FOREIGN KEY `group_fk0`;
+
+ALTER TABLE `group` DROP FOREIGN KEY `group_fk1`;
+
+ALTER TABLE `course_teacher` DROP FOREIGN KEY `course_teacher_fk0`;
+
+ALTER TABLE `course_teacher` DROP FOREIGN KEY `course_teacher_fk1`;
+
+ALTER TABLE `homework` DROP FOREIGN KEY `homework_fk0`;
+
+ALTER TABLE `homework` DROP FOREIGN KEY `homework_fk1`;
+
+ALTER TABLE `lesson` DROP FOREIGN KEY `lesson_fk0`;
+
+ALTER TABLE `schedule` DROP FOREIGN KEY `schedule_fk0`;
+
+ALTER TABLE `schedule` DROP FOREIGN KEY `schedule_fk1`;
+
+ALTER TABLE `schedule` DROP FOREIGN KEY `schedule_fk2`;
+
+ALTER TABLE `lesson_material` DROP FOREIGN KEY `lesson_material_fk0`;
+
