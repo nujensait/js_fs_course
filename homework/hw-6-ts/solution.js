2762f569c84777b74e816708f0e7f0aea17020c7
Mikhail Ikonnikov <mishaikon@gmail.com>
2024-02-29 15:46:43 +0300

    GA JS FS HW-6 TypeScript: add *.ts

---
+++ homework/hw-6-ts/solution.js
@@ -0,0 +1,7 @@
+// Utility Types упрощают создание сложных типов на основе существующих
+// Примеры некоторых полезных Utility Types на TypeScript:
+var views = {
+    home: 10,
+    about: 5,
+};
+var nums = [1, 2, 3];
